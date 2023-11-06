8704f0ff7473dc9495daf41cffa1a8c65ceb8718
Mirza Mehmedagic <mirza.mehmedagic13@gmail.com>
2023-11-06 13:59:35 -0600

    Get full sets of data ready

--- make_urls.py
+++ make_urls.py
@@ -1,28 +1,47 @@
 import json
+from typing import Dict, List
 
-def get_raw_url(orig_url: str) -> str:
-    """
+def get_orig_urls(in_file: str, out_file: str) -> None:
+    orig_urls = []
     
-    As an example:
-    https://github.com/project-baize/baize-chatbot/blob/main/collect.py
-    ...is converted into...
-    https://raw.githubusercontent.com/project-baize/baize-chatbot/main/collect.py
-    """
-    
-    raw_url = orig_url.replace("github.com", "raw.githubusercontent.com").replace("blob/", "")
-    id_index = raw_url.rfind("#")
-    raw_url = raw_url[:id_index]
+    with open(in_file, 'r') as f:
+        data: Dict[str, Dict[str, str]] = json.load(f)
         
-    return raw_url
+        for (_, info) in data.items():
+            orig_url = info.get("file_url")
+            orig_urls.append(orig_url)
+        
+    with open(out_file, 'r+') as f:
+        data = json.load(f)
+        data["orig_urls"] = orig_urls
+        
+        f.seek(0)
+        json.dump(data, f, indent = 4)
+        f.truncate()
+    
+    return orig_urls
 
-def update_url_file(file_name: str) -> None:
+
+def get_raw_urls(file_name: str) -> None:
+    orig_urls = []
+    raw_urls = []
+    
     with open(file_name, 'r') as f:
         data = json.load(f)
         orig_urls = data["orig_urls"]
     
-    raw_urls = []
     for orig_url in orig_urls:
-        raw_urls.append(get_raw_url(orig_url))
+        """
+        As an example:
+        https://github.com/project-baize/baize-chatbot/blob/main/collect.py#L__
+        ...is converted into...
+        https://raw.githubusercontent.com/project-baize/baize-chatbot/main/collect.py
+        """
+        raw_url = orig_url.replace("github.com", "raw.githubusercontent.com").replace("blob/", "")
+        id_index = raw_url.rfind("#")
+        id_index = len(raw_url) if id_index == -1 else id_index
+
+        raw_urls.append(raw_url[:id_index])
     
     with open(file_name, 'r+') as f:
         data = json.load(f)
@@ -32,4 +51,8 @@ def update_url_file(file_name: str) -> None:
         json.dump(data, f, indent = 4)
         f.truncate()
 
-update_url_file("training_data.json")
+
+get_raw_urls("data_holdout.json")
+
+get_orig_urls("code_search/completion_1000.json", "data_all.json")
+get_raw_urls("data_all.json")
