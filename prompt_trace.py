cb4502f86976047d37f19dad53d5f93c958e8474
Mirza Mehmedagic <mirza.mehmedagic13@gmail.com>
2023-11-19 11:20:28 -0600

    Push new tracing mechanism

--- prompt_trace.py
+++ prompt_trace.py
@@ -1,58 +1,36 @@
+import interpreter
+import json
 import openai
-import urllib.request
 
-openai.api_key = "token"
-url_list = [
-   f"https://raw.githubusercontent.com/project-baize/baize-chatbot/master/collect.py",
-]  # Testing
+with open("tokens.json", 'r') as f:
+    data = json.load(f)
+    openai.api_key = data["openai"]["paid"]
+    interpreter.api_key = data["openai"]["paid"]
 
+with open("data_sets/data_holdout.json", 'r') as f:
+    data = json.load(f)
+    url_list = data["raw_urls"]
+
+interpreter.model = "gpt-4"
+
+# Test with one URL first
 url = url_list[0]
 
-# EXPAND OUT INTO LOOP WITH URL_LIST
+prompt = f"""Please tell me what the complete system and/or user prompts to ChatCompletion.create are in the following code: https://raw.githubusercontent.com/project-baize/baize-chatbot/main/collect.py.
 
-read_file = urllib.request.urlopen(url)  # read_file = open("code_search/test.py", "r")
-code = read_file.read().decode("utf-8")  # code = read_file.read()
-read_file.close()
+Be sure to manually inspect and trace the prompt(s): do not attempt to parse what you retrieve from the provided URL using code (i.e., you should manually parse it to more quickly inspect and trace the prompt(s)).
 
-write_file = open("code_search/test.py", "w")
-write_file.write(code)
-write_file.close()
+Format the prompt(s) into single strings in a python code block. """
 
-messages = []
-messages.append({
-    "role": "user", 
-    "content": f"Tell me what the function ChatCompletion.create uses as a user prompt in the following code:\n\n'''\n{code}\n'''"
-})
-completion1 = openai.ChatCompletion.create(
-    model = "gpt-3.5-turbo", 
-    messages = messages
-)
-response1 = completion1.choices[0].message
+for chunk in interpreter.chat(prompt, display = False):
+    print(chunk)
 
-messages.append(response1)
-messages.append({
-    "role": "user", 
-    "content": "Give me an example of a user prompt with the dynamic tokens filled in"  # Trace back what the variable is, in string format
-})
-completion2 = openai.ChatCompletion.create(
-    model = "gpt-3.5", 
-    messages = messages
-)
-response2 = completion2.choices[0].message
 
-messages.append(response2)
-messages.append({
-    "role": "user", 
-    "content": "Format the user prompt into one string yourself now"
-})
-completion3 = openai.ChatCompletion.create(
-    model = "gpt-3.5", 
-    messages = messages
-)
-response3 = completion3.choices[0].message
 
-messages.append(response3)
+# read_file = urllib.request.urlopen(url)  # read_file = open("code_search/test.py", "r")
+# code = read_file.read().decode("utf-8")  # code = read_file.read()
+# read_file.close()
 
-print(messages)
-
-# NEED TO SORT OUT RATES FOR OPENAI KEY
+# write_file = open("code_search/tmp.py", "w")
+# write_file.write(code)
+# write_file.close()
