92f9b57f6b6682443725d93da8b5ef0f35f6857f
Mirza Mehmedagic <mirza.mehmedagic13@gmail.com>
2023-11-19 22:09:33 -0600

    Make basic apparatus for open interpreter tracing

--- prompt_trace.py
+++ prompt_trace.py
@@ -2,6 +2,32 @@ import interpreter
 import json
 import openai
 
+# To format later
+IN_DIR = ""
+IN_FILE = ""
+OUT_DIR = ""
+OUT_FILE = ""
+RET_DIR = ""
+RET_FILE = "code_trace/tmp.json"
+
+
+# Helper functions
+def get_prompt(url: str) -> str:
+    prompt = f"""Please tell me what the complete system and/or user prompts to ChatCompletion.create are in the following code: {url}.
+
+    Save all the code that retrieve from curling the above link only into a file called "tmp.py" in the "code_search" directory that exists in the current working directory.
+
+    Be sure to manually inspect and trace the prompt(s): do not attempt to parse what you retrieve from the provided URL using code (i.e., you should manually parse it to more quickly inspect and trace the prompt(s)).
+
+    Format the prompt(s) into single strings in a Python code block. If you cannot trace the prompts, write N/A into the Python code block."""
+    
+    return prompt
+
+def get_repo_name(i: int) -> str:
+    return "repo" + str(i + 1)  # Placeholder for now
+
+
+# OpenAI and OpenInterpreter parameter handling
 with open("tokens.json", 'r') as f:
     data = json.load(f)
     openai.api_key = data["openai"]["paid"]
@@ -11,26 +37,33 @@ with open("data_sets/data_holdout.json", 'r') as f:
     data = json.load(f)
     url_list = data["raw_urls"]
 
-interpreter.model = "gpt-4"
-
-# Test with one URL first
-url = url_list[0]
-
-prompt = f"""Please tell me what the complete system and/or user prompts to ChatCompletion.create are in the following code: https://raw.githubusercontent.com/project-baize/baize-chatbot/main/collect.py.
-
-Be sure to manually inspect and trace the prompt(s): do not attempt to parse what you retrieve from the provided URL using code (i.e., you should manually parse it to more quickly inspect and trace the prompt(s)).
-
-Format the prompt(s) into single strings in a python code block. """
-
-for chunk in interpreter.chat(prompt, display = False):
-    print(chunk)
+interpreter.model = "gpt-4"  # More certain and better-formatted results
+interpreter.auto_run = True  # Safety not a concern (only curl in shell is actually run)
 
 
+# Prepare new file for writing
+with open(RET_FILE, "w") as f:
+    data = {}
+    
+    for (i, _) in enumerate(url_list):
+        data[get_repo_name(i)] = []
+    
+    json.dump(data, f, indent = 4)
 
-# read_file = urllib.request.urlopen(url)  # read_file = open("code_search/test.py", "r")
-# code = read_file.read().decode("utf-8")  # code = read_file.read()
-# read_file.close()
 
-# write_file = open("code_search/tmp.py", "w")
-# write_file.write(code)
-# write_file.close()
+# Iterate through all URLs
+for (i, url) in enumerate(url_list):
+    # prompt = generate_prompt(url)
+    # print(prompt)
+    # ret = interpreter.chat(prompt, display = False)
+    # print(ret)
+
+    ret = [{"yada": "yada", "yak": "yak"}, {"yada": "yak", "yak": "yada"}] * i
+
+    with open(RET_FILE, "r+") as f:
+        data = json.load(f)
+        data[get_repo_name(i)] = ret
+    
+        f.seek(0)
+        json.dump(data, f, indent = 4)
+        f.truncate()
