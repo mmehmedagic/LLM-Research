3d1ccf2cfc644c011cad02b3b792b62284781a8e
Mirza Mehmedagic <mirza.mehmedagic13@gmail.com>
2023-12-15 17:03:24 -0600

    Continue on initial apparatus (from old November code)

--- prompt_trace.py
+++ prompt_trace.py
@@ -1,25 +1,31 @@
 import interpreter
 import json
 import openai
+import os
+import time
 
-# To format later
-IN_DIR = ""
-IN_FILE = ""
-OUT_DIR = ""
-OUT_FILE = ""
-RET_DIR = ""
-RET_FILE = "code_trace/tmp.json"
+
+CWD = os.getcwd()
+URL_DIR = os.path.join(CWD, "data_sets")
+URL_FILE = "data_holdout.json"
+RET_DIR = os.path.join(CWD, "code_trace")
+RET_FILE = "tmp.json"
 
 
 # Helper functions
 def get_prompt(url: str) -> str:
-    prompt = f"""Please tell me what the complete system and/or user prompts to ChatCompletion.create are in the following code: {url}.
-
-    Save all the code that retrieve from curling the above link only into a file called "tmp.py" in the "code_search" directory that exists in the current working directory.
-
-    Be sure to manually inspect and trace the prompt(s): do not attempt to parse what you retrieve from the provided URL using code (i.e., you should manually parse it to more quickly inspect and trace the prompt(s)).
-
-    Format the prompt(s) into single strings in a Python code block. If you cannot trace the prompts, write N/A into the Python code block."""
+    prompt = """Tell me what the complete system and/or user prompts to the function openai.ChatCompletion.create are in the following code: """ + url + """
+    In the openai.ChatCompletion.create call, there will be the parameter "messages", which will roughly follow this format:
+        [
+            {"role": "system", "content": ...},
+            {"role": "user", "content": ...},
+        ]
+    Here, the two instances of "..." are the system and user prompts, respectively, if they are defined within the messages list. These are what you should be looking for.
+    
+    Save all the code that you may retrieve from curling the above link only into a file called "tmp.py" in the "code_trace" directory that exists in the current working directory.
+    Be sure to manually inspect and trace the prompt(s): do not attempt to parse what you retrieve from the provided URL using code: i.e., you should manually parse it to more quickly inspect and trace the prompt(s).
+    Format the prompt(s) into single strings in a Python code block. If the original string of the prompt is not present in this particular file, write N/A into the Python code block.
+    One direct call to openai.ChatCompletion.create is present in the code, so please inspect thoroughly: there should not be any cause for an N/A result stemming from openai.ChatCompletion.create not being present in the code."""
     
     return prompt
 
@@ -33,37 +39,30 @@ with open("tokens.json", 'r') as f:
     openai.api_key = data["openai"]["paid"]
     interpreter.api_key = data["openai"]["paid"]
 
-with open("data_sets/data_holdout.json", 'r') as f:
+with open(os.path.join(URL_DIR, URL_FILE), 'r') as f:
     data = json.load(f)
     url_list = data["raw_urls"]
 
 interpreter.model = "gpt-4"  # More certain and better-formatted results
 interpreter.auto_run = True  # Safety not a concern (only curl in shell is actually run)
-
-
-# Prepare new file for writing
-with open(RET_FILE, "w") as f:
-    data = {}
-    
-    for (i, _) in enumerate(url_list):
-        data[get_repo_name(i)] = []
-    
-    json.dump(data, f, indent = 4)
+interpreter.conversation_filename = os.path.join(RET_DIR, RET_FILE)
+interpreter.temperature = 0.7
 
 
 # Iterate through all URLs
 for (i, url) in enumerate(url_list):
-    # prompt = generate_prompt(url)
-    # print(prompt)
-    # ret = interpreter.chat(prompt, display = False)
-    # print(ret)
-
-    ret = [{"yada": "yada", "yak": "yak"}, {"yada": "yak", "yak": "yada"}] * i
-
-    with open(RET_FILE, "r+") as f:
-        data = json.load(f)
-        data[get_repo_name(i)] = ret
+    prompt = get_prompt(url)
+    ret = interpreter.chat(prompt, display = True)
+    print(f"Done running: {url}")
     
-        f.seek(0)
-        json.dump(data, f, indent = 4)
-        f.truncate()
+    # time.sleep(30)  # Ensure no rate-limit exit
+    print(f"Done waiting after: {url}")
+    
+    print("\n")
+
+# Format the resulting JSON file correctly
+with open(os.path.join(RET_DIR, RET_FILE), "r+") as f:
+    data = json.load(f)
+    f.seek(0)
+    json.dump(data, f, indent = 4)
+    f.truncate()
