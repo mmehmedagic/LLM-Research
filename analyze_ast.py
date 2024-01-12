f625ed01f38b48e35a4b2f4db2c619796292fad8
Mirza Mehmedagic <mirza.mehmedagic13@gmail.com>
2024-01-12 16:52:31 -0600

    Prepare for further tracing prompts in single file

--- analyze_ast.py
+++ analyze_ast.py
@@ -1,35 +1,55 @@
 import ast  # Documentation: https://docs.python.org/3/library/ast.html
 import json
 import os
+import re
+from typing import Dict, List, Tuple
 
 from utils import Path
 
-with open(os.path.join(Path.URL_DIR, Path.URL_FILE), 'r') as f:
-    data = json.load(f)
-    url_list = data["raw_urls"]
-
-
-with open("code_trace/holdout/tmp2.py", 'r') as f:
-    tree = ast.parse(f.read())
-
-with open("code_trace/ast.txt", 'w') as f:
-    f.write(str(
-        ast.dump(tree, indent = 4)
-    ))
-
-def create_calls(tree):
+def get_create_calls(tree: ast.Module) -> List[Dict[str, str]]:
+    calls = list()
+    
     for statement in ast.walk(tree):
         if isinstance(statement, ast.Call) and isinstance(statement.func, ast.Attribute) and statement.func.attr == 'create':
-            yield {
-                'model': ast.unparse(statement.func.value),
-                'args': [ast.unparse(j) for j in statement.args],
-                'kwargs': {j.arg: ast.unparse(j.value) for j in statement.keywords}
-            }
+            for (i, word) in enumerate(statement.keywords):
+                if word.arg == "messages":
+                    break
+            
+            calls.append({
+                "func": ast.unparse(statement.func.value),
+                "input": word.value
+            })
+            
+    return calls
 
-print([*create_calls(tree)])
+def find_assignment(tree: ast.Module, var_name: str) -> None:
+    print(var_name)
+    
+def find_function_call(tree: ast.Module, func_name: str, param_id: int) -> None:
+    print(func_name)
 
-# for statement in ast.walk(tree):
-#     if isinstance(statement, ast.Call) and isinstance(statement.func, ast.Attribute):
-#         if statement.func.attr == 'create' and isinstance(statement.func.value, ast.Attribute):
-#             if statement.func.value.attr == 'ChatCompletion':
-#                 pass
+# Maybe bring in an ML algorithm to know when to stop?
+def trace_back_input(tree: ast.Module, input) -> None:
+    if type(input) == ast.List:        
+        dict_elements = ast.unparse(input)[1:-1]
+        dict_elements = re.split("}, {|{|}|', '", dict_elements)[1:-1]
+        
+        for elt in input.elts:
+            prompt = elt.values[1]
+            if type(prompt) == ast.Name:
+                find_assignment(tree, prompt.id)
+        
+        print(dict_elements)
+            
+for i in range(9)[0:1]:
+    with open(f"code_trace/holdout/tmp{i}.py", 'r') as f:
+        tree = ast.parse(f.read())
+
+    with open("code_trace/ast.txt", 'w') as f:
+        f.write(str(
+            ast.dump(tree, indent = 4)
+        ))
+    
+    calls = get_create_calls(tree)
+    for call in calls:
+        trace_back_input(tree, call["input"])
